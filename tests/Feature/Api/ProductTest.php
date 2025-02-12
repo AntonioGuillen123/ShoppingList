b07ea1935e5a08bded035d1a45956746163181d1
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-02-12 09:52:01 +0100

    test: Create API Product store test

--- tests/Feature/Api/ProductTest.php
+++ tests/Feature/Api/ProductTest.php
@@ -21,4 +21,41 @@ class ProductTest extends TestCase
             ->assertStatus(200)
             ->assertJsonCount(5);
     }
+
+    public function test_CheckIfPostAnEntryOfProductInJsonFile()
+    {
+        $this->seed(DatabaseSeeder::class);
+
+        $data = [
+            'name' => 'Test Product',
+            'description' => 'Test Description'
+        ];
+
+        $response = $this->postJson(route('createProductAPI'), $data);
+
+        $response
+            ->assertStatus(201)
+            ->assertJsonFragment($data);
+    }
+
+    public function test_CheckIfPostAnEntryOfProductWithExistsNameInJsonFile()
+    {
+        $this->seed(DatabaseSeeder::class);
+
+        $data = [
+            'name' => 'Milk',
+            'description' => 'Test Description'
+        ];
+
+        $response = $this->postJson(route('createProductAPI'), $data);
+
+        $errorData = [
+            'message' => 'The product you have entered already exists :('
+        ];
+
+        $response
+            ->assertStatus(404)
+            ->assertJsonFragment($errorData);
+    }
+
 }
