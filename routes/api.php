dc4448b8a2a88d394271baf633128a70ff71b9ef
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-02-12 09:41:48 +0100

    feat: Add delete list API to routes

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,4 @@ Route::get('/list', [ProductController::class, 'index'])->name('listProductAPI')
 Route::post('/list', [ProductController::class, 'store'])->name('createProductAPI');
 Route::put('/list/{id}', [ProductController::class, 'update'])->name('updateProductAPI');
 Route::delete('/list/{id}', [ProductController::class, 'destroy'])->name('deleteProductAPI');
+Route::delete('/list', [ProductController::class, 'deleteList'])->name('deleteListAPI');
