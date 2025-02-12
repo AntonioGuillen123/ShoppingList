92c584e2ac4bf1c36ca70739c7648dadf49dda69
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-02-12 08:50:02 +0100

    feat: Create API Routes

--- routes/api.php
+++ routes/api.php
@@ -2,4 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Product;
 
+Route::get('/list', [Product::class, 'index'])->name('listProductAPI');
+Route::post('/list', [Product::class, 'store'])->name('createProductAPI');
+Route::put('/list', [Product::class, 'update'])->name('updateProductAPI');
+Route::delete('/list', [Product::class, 'destroy'])->name('deleteProductAPI');
