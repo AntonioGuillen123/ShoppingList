c58da9acd898daabad471dc2e123479d20b2647e
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-02-12 09:28:59 +0100

    fix: Fix routes again 😰

--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,5 @@ use App\Http\Controllers\Api\ProductController;
 
 Route::get('/list', [ProductController::class, 'index'])->name('listProductAPI');
 Route::post('/list', [ProductController::class, 'store'])->name('createProductAPI');
-Route::put('/list', [ProductController::class, 'update'])->name('updateProductAPI');
-Route::delete('/list', [ProductController::class, 'destroy'])->name('deleteProductAPI');
+Route::put('/list/{id}', [ProductController::class, 'update'])->name('updateProductAPI');
+Route::delete('/list/{id}', [ProductController::class, 'destroy'])->name('deleteProductAPI');
